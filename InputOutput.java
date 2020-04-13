982667a0216eadbb64692bac3bd95742564c91d1
AbiYazid <eskrimtemulawak@gmail.com>
2020-04-13 22:29:58 +0800

    Compile & Run

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,20 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int jumlahSKS;
+  double IPK;
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSKS = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara: ");
+  IPK = scanner.nextDouble();
+
+  Double jumlah = IPK * jumlahSKS;
+  System.out.print("Total Nilai = "+jumlah);
  }
 }
